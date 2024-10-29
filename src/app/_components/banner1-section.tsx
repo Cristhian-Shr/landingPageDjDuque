7212f7f7d5e8c6adda6494e8007b4f016cf89c79
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2024-10-29 20:31:36 -0300

    ajuste em banner efeito parallax

--- src/app/_components/banner1-section.tsx
+++ src/app/_components/banner1-section.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import bannerOne from "../../../public/images/banners/banner1.jpg";
+import { url } from "inspector";
 
 export function BannerOne() {
   return (
-    <section className="relative w-full min-h-[90vh] bg-fixed">
-      <Image
+    <section className=" w-full min-h-[90vh] bg-fixed bg-center bg-no-repeat bg-cover" style={{ backgroundImage: "url('/images/banners/banner1.jpg')" }}>
+      {/* <Image
         alt="bannerOne"
         src={bannerOne}
         layout="fill" 
         quality={100}
         objectFit="cover" 
         className="absolute inset-0"
-      />
+      /> */}
+      
     </section>
   );
 }
