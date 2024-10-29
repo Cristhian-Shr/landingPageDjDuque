51f3af677ddfc981a5589bf8c694b33d6026a5cf
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2024-10-29 20:45:50 -0300

    ajuste no efeito parallax

--- src/app/_components/banner2-section.tsx
+++ src/app/_components/banner2-section.tsx
@@ -1,17 +1,8 @@
-import Image from "next/image";
-import bannerTwo from "../../../public/images/banners/banner2.jpg";
-
 export function BannerTwo() {
   return (
-    <section className="relative w-full min-h-[90vh]">
-      <Image
-        alt="bannerOne"
-        src={bannerTwo}
-        layout="fill" 
-        quality={100}
-        objectFit="cover" 
-        className="inset-0 fixed"
-      />
-    </section>
+    <section
+      className=" w-full min-h-[90vh] bg-fixed bg-center bg-no-repeat bg-cover"
+      style={{ backgroundImage: "url('/images/banners/banner2.jpg')" }}
+    ></section>
   );
 }
--- src/app/_components/banner3-section.tsx
+++ src/app/_components/banner3-section.tsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
-import bannerThree from "../../../public/images/banners/banner.jpg";
 
 export function BannerThree() {
   return (
-    <section className="relative w-full min-h-[90vh] bg-fixed">
-      <Image
-        alt="bannerThree"
-        src={bannerThree}
-        layout="fill" 
-        quality={100}
-        objectFit="cover" 
-        className="absolute inset-0"
-      />
-    </section>
+    <section
+      className=" w-full min-h-[90vh] bg-fixed bg-center bg-no-repeat bg-cover"
+      style={{ backgroundImage: "url('/images/banners/banner.jpg')" }}
+    ></section>
   );
 }
