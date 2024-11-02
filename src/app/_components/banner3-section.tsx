152fcc497f49b4d93558a0c460f1b7311b70729c
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2024-11-02 17:17:35 -0300

    ajuste em banner parallax

--- src/app/_components/banner3-section.tsx
+++ src/app/_components/banner3-section.tsx
@@ -1,7 +1,7 @@
 export function BannerThree() {
   return (
     <section
-      className="w-full min-h-[90vh] bg-fixed bg-cover bg-center"
+     className="w-full min-h-[70vh] sm:min-h-[80vh] md:min-h-[90vh] bg-fixed bg-cover bg-center sm:bg-center md:bg-center"
       style={{ backgroundImage: "url('/images/banners/banner7.jpeg')" }}
     ></section>
   );
