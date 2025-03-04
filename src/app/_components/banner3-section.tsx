1913441d1cdf265f292b95542d06e8f2195be7bb
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-04 16:43:11 -0300

    ajustes bg image

--- src/app/_components/banner3-section.tsx
+++ src/app/_components/banner3-section.tsx
@@ -1,6 +1,7 @@
 export function BannerThree() {
   return (
-    <div className="w-full min-h-[60vh] md:min-h-screen bg-fixed bg-cover bg-no-repeat bg-center md:bg-hero3 bg-heroCelThree">
+    <div className="w-full min-h-[70vh] md:min-h-screen bg-fixed md:bg-cover bg-no-repeat md:bg-center md:bg-hero3 bg-heroCelThree bg-contain">
+
     </div>
   );
 }
