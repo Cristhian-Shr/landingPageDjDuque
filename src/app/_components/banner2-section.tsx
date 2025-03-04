1614e4059e5d7c5f1c94e63e0392e62641c12040
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-04 10:11:29 -0300

    ajuste em imagem banner teste

--- src/app/_components/banner2-section.tsx
+++ src/app/_components/banner2-section.tsx
@@ -1,9 +1,9 @@
 export function BannerTwo() {
   return (
     <section
-    className="w-full min-h-[90vh] bg-fixed bg-cover bg-center"
-    style={{ backgroundImage: "url('/images/banners/banner4.jpeg')" }}
-  >
-  </section>
+      className="w-full min-h-[90vh] bg-fixed bg-cover bg-center sm:bg-contain md:bg-cover"
+      style={{ backgroundImage: "url('/images/banners/banner4.jpeg')" }}
+    >
+    </section>
   );
 }
