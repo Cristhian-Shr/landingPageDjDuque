5e3f71aecd85e4064ef60f82af6e4fb7cb2d76fc
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-04 16:36:38 -0300

    novo ajuste imagens banner

--- src/app/_components/banner2-section.tsx
+++ src/app/_components/banner2-section.tsx
@@ -1,6 +1,6 @@
 export function BannerTwo() {
   return (
-    <div className="w-full min-h-[60vh] md:min-h-screen bg-fixed bg-cover bg-no-repeat bg-center md:bg-hero2 bg-heroCelTwo">
+    <div className="w-full min-h-[60vh] md:min-h-screen bg-fixed md:bg-cover bg-contain bg-no-repeat md:bg-center md:bg-hero2 bg-heroCelTwo">
     </div>
   );
 }
