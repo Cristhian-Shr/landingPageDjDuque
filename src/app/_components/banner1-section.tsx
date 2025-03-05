3cc573d77e3f1ceb65ae6c33fd59743e1fb32cbb
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-05 08:39:34 -0300

    ajuste de imagem de background

--- src/app/_components/banner1-section.tsx
+++ src/app/_components/banner1-section.tsx
@@ -1,6 +1,6 @@
 export function BannerOne() {
   return (
-    <div className="w-full min-h-[70vh] md:min-h-screen bg-fixed md:bg-cover bg-no-repeat md:bg-center md:bg-hero1 bg-heroCel bg-contain">
+    <div className="w-full min-h-[70vh] md:min-h-screen bg-fixed bg-cover bg-no-repeat md:bg-center md:bg-hero1 bg-heroCel">
     </div>
   );
 }
