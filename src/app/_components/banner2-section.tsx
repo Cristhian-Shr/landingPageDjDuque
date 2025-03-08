8d9ce1a1e7b25af60f96a0363ef7c50affc78e0c
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-08 16:44:25 -0300

    teste 3

--- src/app/_components/banner2-section.tsx
+++ src/app/_components/banner2-section.tsx
@@ -2,7 +2,7 @@ export function BannerTwo() {
   return (
     // <div className="w-full h-screen min-h-screen bg-fixed bg-contain md:bg-cover bg-center bg-no-repeat bg-heroCelTwo md:bg-hero2">
     // </div>
-    <div className="bg-heroCel lg:bg-hero1 lg:bg-center min-h-screen bg-no-repeat bg-cover w-full">
+    <div className="bg-heroCelTwo lg:bg-hero2 lg:bg-center min-h-screen bg-no-repeat bg-cover w-full">
 
     </div>
   );
--- src/app/_components/banner4-section.tsx
+++ src/app/_components/banner4-section.tsx
@@ -2,7 +2,7 @@ export function BannerFour() {
   return (
     // <div className="w-full min-h-[60vh] md:min-h-screen bg-fixed bg-contain md:bg-cover bg-no-repeat md:bg-center md:bg-hero4 bg-heroCelFour">
     // </div>
-    <div className="bg-heroCel lg:bg-hero1 lg:bg-center min-h-screen bg-no-repeat bg-cover w-full">
+    <div className="bg-heroCelFour lg:bg-hero4 lg:bg-center min-h-screen bg-no-repeat bg-cover w-full">
 
     </div>
   );
