b220e8954ade38b85fb80f069f15a65e53c453d2
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-08 16:38:24 -0300

    ajuste no bg teste1

--- src/app/_components/banner1-section.tsx
+++ src/app/_components/banner1-section.tsx
@@ -1,6 +1,9 @@
 export function BannerOne() {
   return (
-    <div className="w-full min-h-[70vh] md:min-h-screen bg-fixed bg-contain md:bg-cover bg-no-repeat md:bg-center md:bg-hero1 bg-heroCel">
+    // <div className="w-full min-h-[70vh] md:min-h-screen bg-fixed bg-contain md:bg-cover bg-no-repeat md:bg-center md:bg-hero1 bg-heroCel">
+    // </div>
+    <div className="bg-heroCel lg:bg-hero1 lg:bg-center min-h-screen bg-no-repeat bg-cover w-full">
+
     </div>
   );
 }
